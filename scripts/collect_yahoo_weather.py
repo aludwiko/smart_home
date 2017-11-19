0734afe2a172b05ab7bdb0d2699fbd66e9ffb85f
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2017-11-19 18:01:38 +0100

    repeating in case of an exception

--- scripts/collect_yahoo_weather.py
+++ scripts/collect_yahoo_weather.py
@@ -15,40 +15,44 @@ password = "nimdanimda"
 dbname = "smart_home"
 measurement = "yahoo_weather_readings"
 city = "wroclaw"
+repeat = True
 
 def format_float(number):
     return float("%.2f" % round(float(number), 2))
 
-try:
-    result = urllib2.urlopen(yql_url).read()
-    data = json.loads(result)
+while repeat:
+    try:
+        result = urllib2.urlopen(yql_url).read()
+        data = json.loads(result)
 
-    atmosphere = data['query']['results']['channel']['atmosphere']
-    condition = data['query']['results']['channel']['item']['condition']
-    humidity = atmosphere['humidity']
-    pressure = atmosphere['pressure']
-    temp_f = condition['temp']
-    temp_c = (int(temp_f) - 32)/1.8
+        #quering yahoo succeed
+        repeat = False
+        atmosphere = data['query']['results']['channel']['atmosphere']
+        condition = data['query']['results']['channel']['item']['condition']
+        humidity = atmosphere['humidity']
+        pressure = atmosphere['pressure']
+        temp_f = condition['temp']
+        temp_c = (int(temp_f) - 32)/1.8
 
-    # Create the InfluxDB object
-    client = InfluxDBClient(host, port, user, password, dbname)
-    current_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    json_body = [
-        {
-            "measurement": measurement,
-            "tags": {
-                "city": city
-            },
-            "time": current_time,
-            "fields": {
-                "temperature" : format_float(temp_c),
-                "humidity" : format_float(humidity),
-                "pressure" : format_float(pressure)
-            }
-        }]
-    # Write JSON to InfluxDB
-    client.write_points(json_body)
-    print json_body
-except:
-    print "Unexpected error:", sys.exc_info()
+        # Create the InfluxDB object
+        client = InfluxDBClient(host, port, user, password, dbname)
+        current_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        json_body = [
+            {
+                "measurement": measurement,
+                "tags": {
+                    "city": city
+                },
+                "time": current_time,
+                "fields": {
+                    "temperature" : format_float(temp_c),
+                    "humidity" : format_float(humidity),
+                    "pressure" : format_float(pressure)
+                }
+            }]
+        # Write JSON to InfluxDB
+        client.write_points(json_body)
+        print json_body
+    except:
+        print "Unexpected error:", sys.exc_info()
 
